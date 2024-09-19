604b1d2b34f69530320e782aec722254089ed07a
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-09-19 22:19:10 +0300

    finished module-05 lesson-02

--- module-05-lesson-02/js/form.js
+++ module-05-lesson-02/js/form.js
@@ -1,6 +1,13 @@
-const { reviews_form } = document.forms;
-console.log(reviews_form);
+const refs = {
+  modal: document.querySelector('.js-modal'),
+  openModalBtn: document.querySelector('.js-open-modal'),
+  closeModalBtn: document.querySelector('.js-close-modal'),
+};
 
-reviews_form.addEventListener('submit', event => {
-  // event.preventDefault();
-});
+refs.openModalBtn.addEventListener('click', toggleModal);
+refs.closeModalBtn.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  refs.modal.classList.toggle('is-open');
+  document.body.classList.toggle('no-scroll');
+}
